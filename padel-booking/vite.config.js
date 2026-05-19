3e090d461ae173a61f054a7a3c3fca87bcadda45
Satrio Rafli Sangaji <satriorafli1945@gmail.com>
2026-05-19 11:08:16 +0700

    force clean vite config update

--- padel-booking/vite.config.js
+++ padel-booking/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Konfigurasi standar React + Vite (Anti-Crash di Vercel)
 export default defineConfig({
   plugins: [react()],
 })
