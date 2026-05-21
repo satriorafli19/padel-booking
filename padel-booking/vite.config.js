625b629c66bcc33221f96e5705375495f22c5638
satriorafli19 <satriorafli1945@gmail.com>
2026-05-21 08:22:14 +0700

    Update vite.config.js

--- padel-booking/vite.config.js
+++ padel-booking/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
